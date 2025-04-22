d15853ef768b7c61240f0ee3e960c3bbb418c016
DarkBiquette666 <deneyer.anthony.ds@gmail.com>
2025-04-22 11:47:50 +0200

    Hide container parameters if not override

--- modules/DM_Ambiance_UI_Container.lua
+++ modules/DM_Ambiance_UI_Container.lua
@@ -80,6 +80,7 @@ function UI_Container.drawContainerPresetControls(trackIndex, containerIndex)
 end
 
 -- Function to display container settings in the right panel
+-- Function to display container settings in the right panel
 function UI_Container.displayContainerSettings(trackIndex, containerIndex, width)
     local track = globals.tracks[trackIndex]
     local container = track.containers[containerIndex]
@@ -95,18 +96,11 @@ function UI_Container.displayContainerSettings(trackIndex, containerIndex, width
     reaper.ImGui_PushItemWidth(globals.ctx, width * 0.5)
     local rv, newContainerName = reaper.ImGui_InputText(globals.ctx, "Name##detail_" .. containerId, containerName)
     if rv then container.name = newContainerName end
-
+    
     -- Override parent checkbox
     local overrideParent = container.overrideParent
     local rv, newOverrideParent = reaper.ImGui_Checkbox(globals.ctx, "Override Parent Settings##" .. containerId, overrideParent)
     if rv then container.overrideParent = newOverrideParent end
-
-    -- Add explanatory text
-    if container.overrideParent then
-        reaper.ImGui_TextColored(globals.ctx, 0x00AA00FF, "Using container's own settings")
-    else
-        reaper.ImGui_TextColored(globals.ctx, 0x0088FFFF, "Inheriting settings from parent track")
-    end
     
     -- Container preset controls
     UI_Container.drawContainerPresetControls(trackIndex, containerIndex)
@@ -127,7 +121,6 @@ function UI_Container.displayContainerSettings(trackIndex, containerIndex, width
     if #container.items > 0 then
         if reaper.ImGui_CollapsingHeader(globals.ctx, "Imported items (" .. #container.items .. ")##" .. containerId) then
             local itemToDelete = nil
-            
             -- Loop through all items
             for l, item in ipairs(container.items) do
                 reaper.ImGui_Text(globals.ctx, l .. ". " .. item.name)
@@ -136,7 +129,6 @@ function UI_Container.displayContainerSettings(trackIndex, containerIndex, width
                     itemToDelete = l
                 end
             end
-            
             -- Delete the marked item if any
             if itemToDelete then
                 table.remove(container.items, itemToDelete)
@@ -144,120 +136,130 @@ function UI_Container.displayContainerSettings(trackIndex, containerIndex, width
         end
     end
     
-    -- TRIGGER SETTINGS SECTION
-    reaper.ImGui_Separator(globals.ctx)
-    reaper.ImGui_Text(globals.ctx, "Trigger Settings")
-    
-    -- Repetition activation checkbox
-    local useRepetition = container.useRepetition
-    local rv, newUseRepetition = reaper.ImGui_Checkbox(globals.ctx, "Use trigger rate##" .. containerId, useRepetition)
-    if rv then container.useRepetition = newUseRepetition end
-    
-    -- Only show trigger settings if repetition is enabled
-    if container.useRepetition then
-        -- Interval Mode dropdown - different modes for triggering sounds
-        local intervalModes = "Absolute\0Relative\0Coverage\0\0"
-        local intervalMode = container.intervalMode
-        reaper.ImGui_PushItemWidth(globals.ctx, width * 0.5)
-        local rv, newIntervalMode = reaper.ImGui_Combo(globals.ctx, "Interval Mode##" .. containerId, intervalMode, intervalModes)
-        if rv then container.intervalMode = newIntervalMode end
+    if container.overrideParent then
+        -- Afficher le message qui explique que les paramètres propres sont utilisés
+        reaper.ImGui_TextColored(globals.ctx, 0x00AA00FF, "Using container's own settings")
         
-        -- Trigger rate label and slider range changes based on selected mode
-        local triggerRateLabel = "Interval (sec)"
-        local triggerRateMin = -10.0
-        local triggerRateMax = 60.0
+        -- TRIGGER SETTINGS SECTION
+        reaper.ImGui_Separator(globals.ctx)
+        reaper.ImGui_Text(globals.ctx, "Trigger Settings")
         
-        if container.intervalMode == 1 then
-            triggerRateLabel = "Interval (%)"
-            triggerRateMin = 0.1
-            triggerRateMax = 100.0
-        elseif container.intervalMode == 2 then
-            triggerRateLabel = "Coverage (%)"
-            triggerRateMin = 0.1
-            triggerRateMax = 100.0
-        end
+        -- Repetition activation checkbox
+        local useRepetition = container.useRepetition
+        local rv, newUseRepetition = reaper.ImGui_Checkbox(globals.ctx, "Use trigger rate##" .. containerId, useRepetition)
+        if rv then container.useRepetition = newUseRepetition end
         
-        -- Trigger rate slider
-        local triggerRate = container.triggerRate
-        reaper.ImGui_PushItemWidth(globals.ctx, width * 0.5)
-        local rv, newTriggerRate = reaper.ImGui_SliderDouble(globals.ctx, triggerRateLabel .. "##" .. containerId,
-            triggerRate, triggerRateMin, triggerRateMax, "%.1f")
-        if rv then container.triggerRate = newTriggerRate end
-        
-        -- Help text explaining the selected mode
-        if container.intervalMode == 0 then
-            if container.triggerRate < 0 then
-                reaper.ImGui_TextColored(globals.ctx, 0xFFAA00FF, "Negative interval: Items will overlap and crossfade")
-            else
-                reaper.ImGui_TextColored(globals.ctx, 0xFFAA00FF, "Absolute: Fixed interval in seconds")
+        -- Only show trigger settings if repetition is enabled
+        if container.useRepetition then
+            -- Interval Mode dropdown - different modes for triggering sounds
+            local intervalModes = "Absolute\0Relative\0Coverage\0\0"
+            local intervalMode = container.intervalMode
+            reaper.ImGui_PushItemWidth(globals.ctx, width * 0.5)
+            local rv, newIntervalMode = reaper.ImGui_Combo(globals.ctx, "Interval Mode##" .. containerId, intervalMode, intervalModes)
+            if rv then container.intervalMode = newIntervalMode end
+            
+            -- Trigger rate label and slider range changes based on selected mode
+            local triggerRateLabel = "Interval (sec)"
+            local triggerRateMin = -10.0
+            local triggerRateMax = 60.0
+            
+            if container.intervalMode == 1 then
+                triggerRateLabel = "Interval (%)"
+                triggerRateMin = 0.1
+                triggerRateMax = 100.0
+            elseif container.intervalMode == 2 then
+                triggerRateLabel = "Coverage (%)"
+                triggerRateMin = 0.1
+                triggerRateMax = 100.0
             end
-        elseif container.intervalMode == 1 then
-            reaper.ImGui_TextColored(globals.ctx, 0xFFAA00FF, "Relative: Interval as percentage of time selection")
-        else
-            reaper.ImGui_TextColored(globals.ctx, 0xFFAA00FF, "Coverage: Percentage of time selection to be filled")
+            
+            -- Trigger rate slider
+            local triggerRate = container.triggerRate
+            reaper.ImGui_PushItemWidth(globals.ctx, width * 0.5)
+            local rv, newTriggerRate = reaper.ImGui_SliderDouble(globals.ctx, triggerRateLabel .. "##" .. containerId,
+                triggerRate, triggerRateMin, triggerRateMax, "%.1f")
+            if rv then container.triggerRate = newTriggerRate end
+            
+            -- Help text explaining the selected mode
+            if container.intervalMode == 0 then
+                if container.triggerRate < 0 then
+                    reaper.ImGui_TextColored(globals.ctx, 0xFFAA00FF, "Negative interval: Items will overlap and crossfade")
+                else
+                    reaper.ImGui_TextColored(globals.ctx, 0xFFAA00FF, "Absolute: Fixed interval in seconds")
+                end
+            elseif container.intervalMode == 1 then
+                reaper.ImGui_TextColored(globals.ctx, 0xFFAA00FF, "Relative: Interval as percentage of time selection")
+            else
+                reaper.ImGui_TextColored(globals.ctx, 0xFFAA00FF, "Coverage: Percentage of time selection to be filled")
+            end
+            
+            -- Trigger drift slider (randomness in timing)
+            local triggerDrift = container.triggerDrift
+            reaper.ImGui_PushItemWidth(globals.ctx, width * 0.5)
+            local rv, newTriggerDrift = reaper.ImGui_SliderInt(globals.ctx, "Random variation (%)##" .. containerId, triggerDrift, 0, 100, "%d")
+            if rv then container.triggerDrift = newTriggerDrift end
         end
         
-        -- Trigger drift slider (randomness in timing)
-        local triggerDrift = container.triggerDrift
-        reaper.ImGui_PushItemWidth(globals.ctx, width * 0.5)
-        local rv, newTriggerDrift = reaper.ImGui_SliderInt(globals.ctx, "Random variation (%)##" .. containerId, triggerDrift, 0, 100, "%d")
-        if rv then container.triggerDrift = newTriggerDrift end
-    end
-    
-    -- RANDOMIZATION PARAMETERS SECTION
-    reaper.ImGui_Separator(globals.ctx)
-    reaper.ImGui_Text(globals.ctx, "Randomization parameters")
-    
-    -- Pitch randomization checkbox
-    local randomizePitch = container.randomizePitch
-    local rv, newRandomizePitch = reaper.ImGui_Checkbox(globals.ctx, "Randomize Pitch##" .. containerId, randomizePitch)
-    if rv then container.randomizePitch = newRandomizePitch end
-    
-    -- Only show pitch range if pitch randomization is enabled
-    if container.randomizePitch then
-        local pitchMin = container.pitchRange.min
-        local pitchMax = container.pitchRange.max
-        reaper.ImGui_PushItemWidth(globals.ctx, width * 0.7)
-        local rv, newPitchMin, newPitchMax = reaper.ImGui_DragFloatRange2(globals.ctx, "Pitch Range (semitones)##" .. containerId, pitchMin, pitchMax, 0.1, -48, 48)
-        if rv then
-            container.pitchRange.min = newPitchMin
-            container.pitchRange.max = newPitchMax
+        -- RANDOMIZATION PARAMETERS SECTION
+        reaper.ImGui_Separator(globals.ctx)
+        reaper.ImGui_Text(globals.ctx, "Randomization parameters")
+        
+        -- Pitch randomization checkbox
+        local randomizePitch = container.randomizePitch
+        local rv, newRandomizePitch = reaper.ImGui_Checkbox(globals.ctx, "Randomize Pitch##" .. containerId, randomizePitch)
+        if rv then container.randomizePitch = newRandomizePitch end
+        
+        -- Only show pitch range if pitch randomization is enabled
+        if container.randomizePitch then
+            local pitchMin = container.pitchRange.min
+            local pitchMax = container.pitchRange.max
+            reaper.ImGui_PushItemWidth(globals.ctx, width * 0.7)
+            local rv, newPitchMin, newPitchMax = reaper.ImGui_DragFloatRange2(globals.ctx, "Pitch Range (semitones)##" .. containerId, pitchMin, pitchMax, 0.1, -48, 48)
+            if rv then
+                container.pitchRange.min = newPitchMin
+                container.pitchRange.max = newPitchMax
+            end
         end
-    end
-    
-    -- Volume randomization checkbox
-    local randomizeVolume = container.randomizeVolume
-    local rv, newRandomizeVolume = reaper.ImGui_Checkbox(globals.ctx, "Randomize Volume##" .. containerId, randomizeVolume)
-    if rv then container.randomizeVolume = newRandomizeVolume end
-    
-    -- Only show volume range if volume randomization is enabled
-    if container.randomizeVolume then
-        local volumeMin = container.volumeRange.min
-        local volumeMax = container.volumeRange.max
-        reaper.ImGui_PushItemWidth(globals.ctx, width * 0.7)
-        local rv, newVolumeMin, newVolumeMax = reaper.ImGui_DragFloatRange2(globals.ctx, "Volume Range (dB)##" .. containerId, volumeMin, volumeMax, 0.1, -24, 24)
-        if rv then
-            container.volumeRange.min = newVolumeMin
-            container.volumeRange.max = newVolumeMax
+        
+        -- Volume randomization checkbox
+        local randomizeVolume = container.randomizeVolume
+        local rv, newRandomizeVolume = reaper.ImGui_Checkbox(globals.ctx, "Randomize Volume##" .. containerId, randomizeVolume)
+        if rv then container.randomizeVolume = newRandomizeVolume end
+        
+        -- Only show volume range if volume randomization is enabled
+        if container.randomizeVolume then
+            local volumeMin = container.volumeRange.min
+            local volumeMax = container.volumeRange.max
+            reaper.ImGui_PushItemWidth(globals.ctx, width * 0.7)
+            local rv, newVolumeMin, newVolumeMax = reaper.ImGui_DragFloatRange2(globals.ctx, "Volume Range (dB)##" .. containerId, volumeMin, volumeMax, 0.1, -24, 24)
+            if rv then
+                container.volumeRange.min = newVolumeMin
+                container.volumeRange.max = newVolumeMax
+            end
         end
-    end
-    
-    -- Pan randomization checkbox
-    local randomizePan = container.randomizePan
-    local rv, newRandomizePan = reaper.ImGui_Checkbox(globals.ctx, "Randomize Pan##" .. containerId, randomizePan)
-    if rv then container.randomizePan = newRandomizePan end
-    
-    -- Only show pan range if pan randomization is enabled
-    if container.randomizePan then
-        local panMin = container.panRange.min
-        local panMax = container.panRange.max
-        reaper.ImGui_PushItemWidth(globals.ctx, width * 0.7)
-        local rv, newPanMin, newPanMax = reaper.ImGui_DragFloatRange2(globals.ctx, "Pan Range (-100/+100)##" .. containerId, panMin, panMax, 1, -100, 100)
-        if rv then
-            container.panRange.min = newPanMin
-            container.panRange.max = newPanMax
+        
+        -- Pan randomization checkbox
+        local randomizePan = container.randomizePan
+        local rv, newRandomizePan = reaper.ImGui_Checkbox(globals.ctx, "Randomize Pan##" .. containerId, randomizePan)
+        if rv then container.randomizePan = newRandomizePan end
+        
+        -- Only show pan range if pan randomization is enabled
+        if container.randomizePan then
+            local panMin = container.panRange.min
+            local panMax = container.panRange.max
+            reaper.ImGui_PushItemWidth(globals.ctx, width * 0.7)
+            local rv, newPanMin, newPanMax = reaper.ImGui_DragFloatRange2(globals.ctx, "Pan Range (-100/+100)##" .. containerId, panMin, panMax, 1, -100, 100)
+            if rv then
+                container.panRange.min = newPanMin
+                container.panRange.max = newPanMax
+            end
         end
+    else
+        -- Si Override Parent n'est pas coché, afficher un message explicatif
+        reaper.ImGui_TextColored(globals.ctx, 0x0088FFFF, "Inheriting settings from parent track")
+        reaper.ImGui_TextColored(globals.ctx, 0xAAAAAAFF, "Enable 'Override Parent Settings' to customize parameters")
     end
 end
+
 
 return UI_Container
